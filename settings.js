cd27bd84c05e44343bbe564afb76864fb96dacac
malone254 <161203865+malone254@users.noreply.github.com>
2025-05-27 12:53:05 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@
 // @author : Hector 
 // @telegram : http://t.me/official_kango
 // @github : OfficialKango
-// @whatsapp : +233509977126
+// @whatsapp : +254746496906
 
 //----------------------[ KANGO-XMD ]----------------------//
 
@@ -15,7 +15,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'KANGO~kjNhXKSR#q9rY8gQnyEgvr0QoNLYVLE5UKipGXQ9eawqGl5KNWOY' 
 //Enter your KANGO-XMD session id here; must start with KANGO~
 
 //--------------------[ BOT NAME ]----------------------//
